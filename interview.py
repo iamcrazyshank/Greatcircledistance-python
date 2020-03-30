65f677855ada7f6bb88d3035fc68fed41a4e7dae
Shashank Chandran <iamcrazyshank@gmail.com>
2020-03-30 15:17:08 +0100

    customer list fetch done

--- interview.py
+++ interview.py
@@ -2,3 +2,10 @@
 ## take-home tests
 
 from math import radians, degrees, sin, cos, asin, acos, sqrt
+
+def readfile():
+    cust_file = open("customers.txt")
+    for line in cust_file.readlines():
+        print (line)
+
+readfile()
