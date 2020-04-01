ca2cc18740832116182a7f045ab9356222c2726f
Shashank Chandran <iamcrazyshank@gmail.com>
2020-04-01 20:27:43 +0100

    getting nearby users

--- interview.py
+++ interview.py
@@ -29,25 +29,46 @@ class UserData:
         self.__latitude = lat
 
     def get_latitude(self):
-        return self.__latitude
+        return float(self.__latitude)
 
     def set_longitude(self, long):
         self.__longitude = long
+
+    def get_longitude(self):
+        return float(self.__longitude)
 
     def __str__(self):
         return ("Object: " + str(self.__latitude) + " " + str(self.__longitude) + " " + str(self.__user_name)+ " " + str(self.__user_id))
 
 
+def great_circle(userdara):
+    Dub_off_Lat = 53.339428
+    Dub_off_Long = -6.257664
+    lat=userdara.get_latitude()
+    longi=userdara.get_longitude()
+    Dub_off_Long, Dub_off_Lat, lon2, lat2 = map(radians, [Dub_off_Long, Dub_off_Lat,longi,lat])
+
+    return True if (6371 * ( acos(sin(Dub_off_Lat) * sin(lat2) + cos(Dub_off_Lat) * cos(lat2) * cos(Dub_off_Long - lon2)))) < 100 else False
+
+
+def getnearbycustomers():
+    for user in userObjList:
+            print(great_circle(user))
 
 ## function to read the file
 def readfile():
     users = []
     for line in open('customers.txt', 'r'):
         users.append(json.loads(line))
-    userObjList=[]
     for user in users:
         userObjList.append(UserData(user['user_id'],user['name'],user['latitude'],user['longitude']))
-    print(userObjList[0])
+    
+    getnearbycustomers()
 
 
+userObjList=[]
 readfile()
+
+
+        
+
