8239e18f757ec6d75375fdbc61c9e42412e1ca60
Shashank Chandran <iamcrazyshank@gmail.com>
2020-04-23 20:01:16 +0100

    test cases definition

--- interview_test.py
+++ interview_test.py
@@ -1,9 +1,15 @@
+## Intercom
+## take-home tests
+##Shashank Chandran
+##shashankchandran1994@gmail.com
+
 import unittest
 import sys
 from interview import great_circle
 from interview import checkvalid_lat_long
 from interview import UserData
-import random
+from interview import readfile
+
 
 
 
@@ -17,18 +23,24 @@ class UserDataTest(unittest.TestCase):
         self.test_long_1 = -442.555545
         self.test_lat_2 = 53.2451022
         self.test_long_2 = -6.433333
-
+    #Test 1 IO Error for input file name
     def test_file_directory(self):
-        self.assertEqual(self.inputfile_name,"customers.txt"), "Input file found"
-        
-    def test_checkvalid_lat_long(self):
+        testfilename = "test_fail.txt"
+        self.assertRaises(IOError, readfile(testfilename,self.outputfile_name))
+
+    #Test 2 Valid Lat Long Fail
+    def test_checkvalid_lat_long_Fail(self):
         assert checkvalid_lat_long(self.test_lat_1,self.test_long_1) == True
+
+    #Test 3 Valid Lat Long Fail
+    def test_checkvalid_lat_long_Pass(self):
         assert checkvalid_lat_long(self.test_lat_2,self.test_long_2) == True
 
+    #Test 4 check distance great circle formula- Pass case
     def test_great_cirle_Pass(self):
         test_object = self.userData_testObj_2
         assert great_circle(test_object) == True, "Test User 1 PASS"
-
+    #Test 5 check distance great circle formula- Fail Case
     def test_great_cirle_Fail(self):
         test_object = self.userData_testObj_1
         assert great_circle(test_object) == False, "Test User 2 FAIL"
